56253a6a0d2b2c619b95c0b71b4f5f9a4278cfd9
DangBK <dangbk@fpt.com>
2026-09-09 18:08:30 +0700

    feat: setup backend

---
+++ backend/jurisHub/jurisHub/src/main/java/com/example/jurisHub/JurisHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.jurisHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurisHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurisHubApplication.class, args);
+	}
+
+}
---
+++ backend/jurisHub/jurisHub/src/test/java/com/example/jurisHub/JurisHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jurisHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurisHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
