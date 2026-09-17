d21d474a08fb5d8c7076b7f381a722308ade236f
DangBK <dangbk@fpt.com>
2026-09-17 18:13:27 +0700

    feat: setup angular frontend for project

--- backend/jurisHub/src/main/java/com/example/jurisHub/service/ForumService.java
+++ backend/jurisHub/src/main/java/com/example/jurisHub/service/ForumService.java
@@ -2,10 +2,8 @@ package com.example.jurisHub.service;
 
 import com.example.jurisHub.dto.forum.PostCategoryDto;
 import com.example.jurisHub.dto.forum.PostDto;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
