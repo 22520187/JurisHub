8680c8fc1034d3fca5417b7a351de190fda88fe9
DangBK <dangbk@fpt.com>
2026-09-18 16:29:13 +0700

    feat: UI register page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './features/auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: RegisterComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '' }
+];
